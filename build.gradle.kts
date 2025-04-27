ce1971ba135286584945bda0db3c857c2e6153be
donghwan <ydhwan0905@gmail.com>
2025-04-27 17:38:00 +0900

    chore: 낙관적락 retry 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,16 @@ dependencies {
 	//Lombok
 	implementation("org.projectlombok:lombok:1.18.24")
 	annotationProcessor("org.projectlombok:lombok:1.18.24")
+
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
 	//Swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+	// retry 적용
+	implementation ("org.springframework.retry:spring-retry")
+	implementation ("org.springframework:spring-aspects")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
