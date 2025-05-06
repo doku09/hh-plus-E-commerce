85f8be4bd7d8d2d104b4a7490eccb1aeff75d9b1
donghwan <ydhwan0905@gmail.com>
2025-05-06 19:13:32 +0900

    build: 시간별 인기상품 조회를 위한 주문 생성 스케줄러 추가

--- docs/docs/동시성테스트/dml/insertProduct.sql
+++ docs/docs/동시성테스트/dml/insertProduct.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO product (product_id, name, price, created_at, updated_at) VALUES
                                                                                     (1, '아메리카노', 2000, NOW(), NOW()),
                                                                                     (2, '카페라떼', 2500, NOW(), NOW()),
@@ -9,3 +10,17 @@ INSERT INTO product (product_id, name, price, created_at, updated_at) VALUES
                                                                                     (8, '콜드브루', 2700, NOW(), NOW()),
                                                                                     (9, '모카라떼', 3400, NOW(), NOW()),
                                                                                     (10, '디카페인 아메리카노', 2100, NOW(), NOW());
+
+
+INSERT INTO product_stock (product_id, quantity,version) VALUES (1, 1000,0),
+                                                                (2, 1000,0),
+                                                                (3, 1000,0),
+                                                                (4, 1000,0),
+                                                                (5, 1000,0),
+                                                                (6, 1000,0),
+                                                                (7, 1000,0),
+                                                                (8, 1000,0),
+                                                                (9, 1000,0),
+                                                                (10, 1000,0);
+
+delete from product_stock where id in (1,2,3,4,5,6,7,8,9,10);
---
+++ src/main/java/kr/hhplus/be/server/application/order/OrderScheduler.java
@@ -0,0 +1,59 @@
+package kr.hhplus.be.server.application.order;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderScheduler {
+
+	private final OrderFacade orderFacade;
+	private final Random random = new Random();
+	@Scheduled(cron = "*/10 * * * * *") // 10초마다 실행
+	public void createRandomOrder() {
+		long userId = getRandomUserId();
+		Long couponId = maybeGetCouponId();
+		List<OrderCriteria.OrderItem> items = generateRandomOrderItems();
+
+		OrderCriteria.CreateOrder order = OrderCriteria.CreateOrder.of(userId, couponId, items);
+			log.info("=================");
+		for (OrderCriteria.OrderItem orderItem : order.getOrderItems()) {
+			log.info("주문상품: orderItem:{}",orderItem.getProductId());
+		}
+			log.info("=================");
+		// 주문 처리 서비스 호출 예시
+		OrderResult.Order ordered = orderFacade.order(order);
+	}
+
+	private long getRandomUserId() {
+		return random.nextInt(10) + 1; // 1~100번 사용자
+	}
+
+	private Long maybeGetCouponId() {
+		return null;
+	}
+
+	private List<OrderCriteria.OrderItem> generateRandomOrderItems() {
+		int itemCount = random.nextInt(3) + 1; // 1~3개 상품 주문
+		List<OrderCriteria.OrderItem> items = new ArrayList<>();
+		for (int i = 0; i < itemCount; i++) {
+			long productId = random.nextInt(10) + 1; // 1~10번 상품
+			int quantity = random.nextInt(5) + 1; // 수량 1~5
+			items.add(OrderCriteria.OrderItem.of(productId, quantity));
+		}
+		return items;
+	}
+
+	@PostConstruct
+	private void init() {
+		log.info("주문 초기화");
+	}
+}
